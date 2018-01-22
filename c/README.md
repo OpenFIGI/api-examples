411e140d0b005dff6649918f8d996f532dc2b39f
fiorda <fiorda.f@gmail.com>
2018-01-22 07:41:11 -0500

    c

---
+++ c/README.md
@@ -0,0 +1,5 @@
+
+```
+docker build . -t example
+docker run example --rm
+```
---
+++ c/src/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cJSON.h>
+#include <assert.h>
+
+#include "mapping_job.h"
+#include "api_response.h"
+
+
+int main() {
+    const size_t NUM_JOBS = 3;
+    size_t i = 0, j = 0;
+    char *value = NULL;
+
+    OpenFIGI_API_Response response;
+    OpenFIGI_API_JobResponse jobResponse;
+    OpenFIGI_API_MappingJob *job;
+    OpenFIGI_API_MappingJob *request[NUM_JOBS];
+    OpenFIGI_API_RC rc;
+    cJSON *parsedResponse, *obj, *obj1;
+
+    job = OpenFIGI_API_CreateMappingJob(OpenFIGI_API_ID_WERTPAPIER, "851399");
+    job->exchCode = "US";
+    request[0] = job;
+
+    job = OpenFIGI_API_CreateMappingJob(OpenFIGI_API_ID_BB_UNIQUE, "EQ0010080100001000");
+    job->currency = "USD";
+    request[1] = job;
+
+    job = OpenFIGI_API_CreateMappingJob(OpenFIGI_API_ID_SEDOL, "EQ0010080100001000");
+    job->micCode = "EDGX";
+    job->currency = "USD";
+
+    request[2] = job;
+
+    response = OpenFIGI_API_SendRequestParsed((const OpenFIGI_API_MappingJob **)request, NUM_JOBS, NULL);
+
+    assert(response.numJobs == NUM_JOBS);
+
+    for (i = 0; i < NUM_JOBS; ++i) {
+        jobResponse = response.jobs[i];
+        printf("Job %d had %d results \n\n", i, jobResponse.numResults);
+        for (j = 0; j < jobResponse.numResults; ++j) {
+            OpenFIGI_API_printResult(&jobResponse.results[j]);
+            printf("\n");
+        }
+        printf("\n========\n\n");
+    }
+
+    OpenFIGI_API_DeleteResponse(response);
+
+    for (i = 0; i < NUM_JOBS; ++i) {
+        OpenFIGI_API_DeleteMappingJob(request[i]);
+    }
+
+    return 0;
+}
